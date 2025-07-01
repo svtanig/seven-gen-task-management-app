8f81741132bcb5a01dfa962765081a14be9c3686
Stephanie Tanig <stephanietanig@Stephanies-Air.lan>
2025-07-01 15:17:28 +0800

    create task model

--- models/task.py
+++ models/task.py
@@ -1,1 +1,17 @@
 #Task attributes and logic
+
+import datetime
+import uuid
+
+class Task:
+    def __init__(self, title, description, due_date, priority):
+        self.task_id = str(uuid.uuid4())
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.priority = priority
+        self.status = "Pending"
+        self.created_at = datetime.datetime.now()
+
+    def to_dict(self):
+        return self.__dict__
