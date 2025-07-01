43e78740419b414766306e598be410ac0206641e
Stephanie Tanig <stephanietanig@Stephanies-Air.lan>
2025-07-01 15:18:52 +0800

    create controller and db handler

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -1,1 +1,26 @@
 #Connect db and cli
+
+from database.db_handler import DBHandler
+from models.task import Task
+
+class TaskController:
+    def __init__(self):
+        self.db = DBHandler()
+
+    def create_task(self, title, desc, due, priority):
+        task = Task(title, desc, due, priority)
+        self.db.add_task(task)
+
+    def list_tasks(self, filter_type=None, filter_value=None):
+        if filter_type:
+            return self.db.list_tasks({filter_type: filter_value})
+        return self.db.list_tasks()
+
+    def update_task(self, task_id, updates):
+        self.db.update_task(task_id, updates)
+
+    def delete_task(self, task_id):
+        self.db.delete_task(task_id)
+
+    def mark_completed(self, task_id):
+        self.db.mark_completed(task_id)
--- database/db_handler.py
+++ database/db_handler.py
@@ -1,1 +1,29 @@
 #Database operations (CRUD)
+
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+from bson import ObjectId
+
+load_dotenv()
+
+class DBHandler:
+    def __init__(self):
+        self.client = MongoClient(os.getenv("MONGO_URI"))
+        self.db = self.client[os.getenv("DB_NAME")] #from .env
+        self.collection = self.db["tasks"]
+
+    def add_task(self, task):
+        self.collection.insert_one(task.to_dict())
+
+    def list_tasks(self, filters={}):
+        return list(self.collection.find(filters))
+
+    def update_task(self, task_id, updates):
+        self.collection.update_one({"_id": ObjectId(task_id)}, {"$set": updates})
+
+    def delete_task(self, task_id):
+        self.collection.delete_one({"_id": ObjectId(task_id)})
+
+    def mark_completed(self, task_id):
+        self.update_task(task_id, {"status": "Completed"})
